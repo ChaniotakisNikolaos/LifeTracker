4cacdbcfd438da9c8e46696302db4ace0075fa29
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-05-17 15:20:11 +0300

    Tabs and fragments(Big commit)

---
+++ app/src/main/java/com/example/lifetracker/FragmentAdapter.java
@@ -0,0 +1,36 @@
+package com.example.lifetracker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        //Fragment[] fragments = new Fragment[]{new ToDoFragment(),new CalendarFragment(), new BudgetFragment(), new MeFragment()};
+        //return fragments[position-1];
+        System.out.println("position="+position);
+        switch (position) {
+            case 0:
+                return new ToDoFragment();
+            case 1:
+                return new CalendarFragment();
+            case 2:
+                return new BudgetFragment();
+            default:
+                return new MeFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
--- app/src/main/java/com/example/lifetracker/MainActivity.java
+++ app/src/main/java/com/example/lifetracker/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.lifetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private FragmentAdapter fragmentAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager2 = findViewById(R.id.viewPager2);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentAdapter = new FragmentAdapter(fragmentManager,getLifecycle());
+        viewPager2.setAdapter(fragmentAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
