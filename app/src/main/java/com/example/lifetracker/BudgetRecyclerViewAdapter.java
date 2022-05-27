c2d53090239a6ec9ad85fbf69647c11f77171d9f
SofiaBili <sofia.bili@yahoo.gr>
2022-05-27 16:08:20 +0300

    fix bar

--- app/src/main/java/com/example/lifetracker/BudgetRecyclerViewAdapter.java
+++ app/src/main/java/com/example/lifetracker/BudgetRecyclerViewAdapter.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -39,21 +41,22 @@ public class BudgetRecyclerViewAdapter extends RecyclerView.Adapter<BudgetRecycl
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
-        TextView budgetLabel, savingsLabel, progressBar, percentageLabel, dueDate, button, addButton, minusButton, editButton, deleteButton;
-
+        TextView budgetLabel, savingsLabel, percentageLabel, dueDate;
+        ProgressBar progressBar;
+        Button button;
+        FloatingActionButton addButton, minusButton, editButton, deleteButton;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             budgetLabel = itemView.findViewById(R.id.budgetLabel);
             savingsLabel = itemView.findViewById(R.id.savingsLabel);
             dueDate = itemView.findViewById(R.id.dueDate1);
-            /*progressBar = itemView.findViewById(R.id.progressBar);
+            progressBar = itemView.findViewById(R.id.progressBar);
             percentageLabel = itemView.findViewById(R.id.percentageLabel);
-            dueDate = itemView.findViewById(R.id.dueDate);
-            button = itemView.findViewById(R.id.button);
+            button = itemView.findViewById(R.id.buttonClick);
             addButton = itemView.findViewById(R.id.addButton);
             minusButton = itemView.findViewById(R.id.minusButton);
             editButton = itemView.findViewById(R.id.editButton);
-            deleteButton = itemView.findViewById(R.id.deleteButton);*/
+            deleteButton = itemView.findViewById(R.id.deleteButton);
         }
     }
 }
