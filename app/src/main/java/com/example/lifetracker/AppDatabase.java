9dbf8e4864aa8c644b94c1aa60a0e5eac7a11e3a
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-06-23 19:50:20 +0300

    COMMENT AppDatabase.java

--- app/src/main/java/com/example/lifetracker/AppDatabase.java
+++ app/src/main/java/com/example/lifetracker/AppDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {ToDoItem.class, BudgetItem.class}, version = 1, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
+public abstract class AppDatabase extends RoomDatabase { //singleton pattern
 
     public static final String DB_NAME = "life-tracker-db";
     private static AppDatabase instance;
@@ -15,6 +15,6 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract Dao dao();
 
     public static synchronized AppDatabase getInstance(Context context) {
-        return instance==null? instance=Room.databaseBuilder(context,AppDatabase.class,DB_NAME).build():instance;
+        return instance == null ? instance = Room.databaseBuilder(context, AppDatabase.class, DB_NAME).build() : instance;
     }
 }
