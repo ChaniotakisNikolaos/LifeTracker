890dc489a1accdcfdbdd3fd790bf25bd1208e8d7
ChaniotakisNikolaos <nxnick26@gmail.com>
2022-06-23 17:59:11 +0300

    UPDATED AddBudgetItemActivity.java

--- app/src/main/java/com/example/lifetracker/AddBudgetItemActivity.java
+++ app/src/main/java/com/example/lifetracker/AddBudgetItemActivity.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
