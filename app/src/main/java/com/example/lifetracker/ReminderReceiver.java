992495fe5ac440651e60c595dedbbf9de6281e9e
SofiaBili <sofia.bili@yahoo.gr>
2022-06-24 17:20:36 +0300

    optimize ReminderReceiver.java

--- app/src/main/java/com/example/lifetracker/ReminderReceiver.java
+++ app/src/main/java/com/example/lifetracker/ReminderReceiver.java
@@ -3,7 +3,6 @@ package com.example.lifetracker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
