0d257610ee4b21e20d0152b34a34433efd594c55
SofiaBili <sofia.bili@yahoo.gr>
2022-06-24 18:08:14 +0300

    comment ToDoFragment

--- app/src/main/java/com/example/lifetracker/ToDoFragment.java
+++ app/src/main/java/com/example/lifetracker/ToDoFragment.java
@@ -53,11 +53,15 @@ public class ToDoFragment extends Fragment {
         showAllToDos(toDoRecyclerViewAdapter);//display all todos and observe them
         toDoRecyclerViewAdapter.setApplicationViewModel(applicationViewModel);
 
+        //get navigation drawer
         NavigationView navView = (requireActivity()).findViewById(R.id.menu_navigation);
         m = navView.getMenu();
+        //listener for the menu item the user clicked
         navView.setNavigationItemSelectedListener(menuItem -> {
             currentId = menuItem.getItemId();
+            //if the menu item was not already checked
             if (!menuItem.isChecked()) {
+                //for all the menu items find which menu item was previously checked in order to uncheck it and change the icon
                 for (int mId = 0; mId < m.size(); mId++) {
                     if (m.getItem(mId).isChecked()) {
                         m.getItem(mId).setChecked(false);
@@ -65,17 +69,23 @@ public class ToDoFragment extends Fragment {
                         break;
                     }
                 }
+                //check the current menu icon and change icon to show that the user is here
                 menuItem.setChecked(true);
                 menuItem.setIcon(R.drawable.ic_baseline_current_label_24);
             }
+            //if the user has clicked at a menu item other than All to do, then get the name of that menu item
+            //because it will be the label of the to dos that must appear
             if (currentId != 0) {
-                labelName = menuItem.getTitle().toString();
+                labelName = menuItem.getTitle().toString();//get label
                 applicationViewModel.getAllToDoItemsWithLabel(labelName).observe(getViewLifecycleOwner(), toDoItems -> {
+                    //because the observe is always running on any change, we must check that we are not at the first menu item(all to dos)
+                    //before showing all the to dos with that label
                     if (!m.getItem(0).isChecked()) {
                         toDoRecyclerViewAdapter.submitList(toDoItems);
                     }
                 });
             } else {
+                //if the user is at all to dos then show all
                 showAllToDos(toDoRecyclerViewAdapter);
             }
             return false;
@@ -123,9 +133,12 @@ public class ToDoFragment extends Fragment {
 
     public void showAllToDos(ToDoRecyclerViewAdapter toDoRecyclerViewAdapter) {
         applicationViewModel.getToDoItemList().observe(getViewLifecycleOwner(), toDoItems -> {
+            //because observe runs every time there is a change, we must make sure that the submitList will only run when the checked menu item
+            //is the first one
             if (m.getItem(0).isChecked()) {
                 toDoRecyclerViewAdapter.submitList(toDoItems);
             }
+            //check if there is a menu item for every label
             checkIfExistsInMenu(toDoItems);
         });
     }
@@ -136,36 +149,47 @@ public class ToDoFragment extends Fragment {
         ((MainActivity) requireActivity()).setDrawerUnlocked();
     }
 
+    /**
+     * check if every label exists in the menu item
+     */
     public void checkIfExistsInMenu(List<ToDoItem> toDoItems) {
         boolean menuFlag = false;
         String toDoLabel;
+        //for all the to dos, get the labels and for every label that is not empty
+        //check if there is a menu item with that name
         for (ToDoItem todo : toDoItems) {
             toDoLabel = todo.getLabel();
             if (!toDoLabel.isEmpty()) {
                 for (int i = 0; i < m.size(); i++) {
+                    //if there is a menu item for that label then make the flag false
                     if (m.getItem(i).getTitle().equals(toDoLabel)) {
                         menuFlag = false;
-                        break;
+                        break;//stop the for
                     } else {
                         menuFlag = true;
                     }
                 }
-                if (menuFlag) {
+                if (menuFlag) {//if no menu item matched to a label then add a new menu item with the same name as the label
                     m.add(Menu.NONE, labelID, Menu.NONE, toDoLabel).setIcon(R.drawable.ic_baseline_label_24);
-                    labelID++;
+                    labelID++;//add 1 to the label id for the next to do label
                 }
             }
         }
-        checkIfExistsInLabel(toDoItems);
+        checkIfExistsInLabel(toDoItems);//check if there is a menu item with a label that is not in the to dos(because the label was deleted)
     }
 
+    /**
+     * Check if there is a menu item with a name that does not correspond to a label, in order to delete the menu item
+     */
     public void checkIfExistsInLabel(List<ToDoItem> toDoItems) {
         boolean menuFlag = false;
         boolean isUserCurrentlyOn;
         String toDoLabel;
+        //for all teh menu items check if there is a label that corresponds to that name
         for (int i = 1; i < m.size(); i++) {
             for (ToDoItem todo : toDoItems) {
                 toDoLabel = todo.getLabel();
+                //if there is a label with the same name as a menu item then keep it
                 if (m.getItem(i).getTitle().equals(toDoLabel)) {
                     menuFlag = false;
                     break;
@@ -173,10 +197,11 @@ public class ToDoFragment extends Fragment {
                     menuFlag = true;
                 }
             }
-            if (menuFlag) {
+            if (menuFlag) {//if there is a menu item without a corresponding label then remove it
                 isUserCurrentlyOn = m.getItem(i).isChecked();
                 m.removeItem(m.getItem(i).getItemId());
-                if (isUserCurrentlyOn) {
+                if (isUserCurrentlyOn) {//if the user was at the current menu item that is deleted
+                    // take him to all to dos and make it current
                     m.getItem(0).setIcon(R.drawable.ic_baseline_current_label_24).setChecked(true);
                 }
             }
