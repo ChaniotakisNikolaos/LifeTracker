b53019a14ab6cf9bd669ef71b8e135fefe553393
SofiaBili <sofia.bili@yahoo.gr>
2022-06-24 17:04:53 +0300

    comment drawer

--- app/src/main/java/com/example/lifetracker/MyDrawerControllerInterface.java
+++ app/src/main/java/com/example/lifetracker/MyDrawerControllerInterface.java
@@ -1,6 +1,7 @@
 package com.example.lifetracker;
 
+/*An interface to lock and unlock the navigation drawer for some fragments */
 public interface MyDrawerControllerInterface {
-    public void setDrawerLocked();
-    public void setDrawerUnlocked();
+    void setDrawerLocked();
+    void setDrawerUnlocked();
 }
